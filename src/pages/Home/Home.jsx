ea9559e67f5862666985497563c116090471db23
kirmann <k.dkle@hotmail.fr>
2022-03-20 22:11:37 +0100

    section photo home ok

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,26 @@
 import React from "react"
+import bankTree from "../../assets/bank-tree.jpeg"
 import "./home.scss"
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <section className="homeSection">
+      <img src={bankTree} alt="plant" />
+      <div className="textContainer">
+        <h2>
+          No fees.
+          <br />
+          No minimum deposit.
+          <br />
+          High interest rates.
+        </h2>
+        <p>
+          Open a savings account with <br />
+          Argent Bank today!
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default Home
