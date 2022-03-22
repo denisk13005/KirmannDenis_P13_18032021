da41659188ca035c5fdd770e8741380251c57da8
kirmann <k.dkle@hotmail.fr>
2022-03-22 11:44:44 +0100

    ntégration de sigout dans le header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { NavLink } from "react-router-dom"
 import argentBankLogo from "../../assets/argentBankLogo.png"
 import SignIn from "../../pages/SignIn/SignIn"
@@ -6,15 +6,34 @@ import SignIn from "../../pages/SignIn/SignIn"
 import "./header.scss"
 
 const Header = () => {
+  const [logged, setLogged] = useState(true)
+  const [userName, setUserName] = useState("Tony")
+  //useSelector(logged,userName)
+
+  const logOut = () => {
+    //vider le local storage peut être
+    window.location = "/"
+  }
   return (
     <header>
       <NavLink to="/">
         <img src={argentBankLogo} alt="bank logo" />
       </NavLink>
-      <NavLink className="signIn" to="/signin">
-        <i className="fa fa-user-circle"></i>
-        Sign In
-      </NavLink>
+      {!logged ? (
+        <NavLink className="signIn" to="/signin">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </NavLink>
+      ) : (
+        <div className="signOut">
+          <i className="fa fa-user-circle"></i>
+          <p>{userName}</p>
+          <div className="logOut" onClick={logOut}>
+            <i className="fa fa-sign-out"></i>
+            <p>Sign Out</p>
+          </div>
+        </div>
+      )}
     </header>
   )
 }
