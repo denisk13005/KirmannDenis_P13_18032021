b5a62fda4314cadadb4bd5d641d81ce34a99483c
kirmann <k.dkle@hotmail.fr>
2022-03-22 22:10:22 +0100

    création des champs input et des btns editnam

--- src/components/EditUserName/EditUserName.jsx
+++ src/components/EditUserName/EditUserName.jsx
@@ -5,12 +5,12 @@ const EditUserName = () => {
   return (
     <div className="editUserNameContainer">
       <div className="fieldContainer">
-        <input type="text" id="lastName" />
-        <input type="text" id="firstName" />
+        <input type="text" id="lastName" placeholder="Tony" color="red" />
+        <input type="text" id="firstName" placeholder="Jarvis" />
       </div>
       <div className="btnsContainer">
-        <button></button>
-        <button></button>
+        <button className="btn saveBtn">Save</button>
+        <button className="btn cancelBtn">Cancel</button>
       </div>
     </div>
   )
