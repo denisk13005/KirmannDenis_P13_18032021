a7842fd33bfff848c954135dffaf2cfe902d466d
kirmann <k.dkle@hotmail.fr>
2022-03-27 16:36:01 +0200

    implémentation du asyncthunk user

--- src/features/user.js
+++ src/features/user.js
@@ -1,11 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import axios from "axios"
+import { baseURL } from "../utils/axios"
 
+export const fetchUserDatas = createAsyncThunk(
+  "user/fetchUserDatas",
+  async (token) => {
+    let res = await axios({
+      method: "post",
+      url: `${baseURL}/user/profile`,
+      headers: {
+        authorization: `Bearer ${token.data.body.token}`,
+      },
+    }).catch((err) => console.log(err))
+    return res.data.body
+  }
+)
 const userSlice = createSlice({
   name: "user",
   initialState: {},
   reducers: {
     getUserDatas: (state, action) => {
-      state = state.payload
+      state = action.payload
       return state
     },
   },
