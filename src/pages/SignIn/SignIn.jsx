5698047bcefd45745aa6c3adc81146376fa98296
kirmann <k.dkle@hotmail.fr>
2022-03-27 17:32:22 +0200

    modif signin

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -24,44 +24,36 @@ const SignIn = () => {
   }
 
   return (
-    <>
-      {isAuth ? (
-        <User />
-      ) : (
-        <div className="main">
-          <div className="signInContainer">
-            <i className="fa fa-user-circle sign-in-icon"></i>
-            <h1>Sign In</h1>
-            <form onSubmit={handleSubmit}>
-              <div className="input">
-                <label htmlFor="usename">Username</label>
-                <input
-                  type="text"
-                  id="username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </div>
-              <div className="input">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div className="checkbox">
-                <input type="checkbox" id="checkbox" />
-                <label htmlFor="checkbox">Remember me</label>
-              </div>
-
-              <input className="signInBtn" type="submit" value="Sign In" />
-            </form>
+    <div className="main">
+      <div className="signInContainer">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input">
+            <label htmlFor="usename">Username</label>
+            <input
+              type="text"
+              id="username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
-        </div>
-      )}
-    </>
+          <div className="input">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="checkbox">
+            <input type="checkbox" id="checkbox" />
+            <label htmlFor="checkbox">Remember me</label>
+          </div>
+
+          <input className="signInBtn" type="submit" value="Sign In" />
+        </form>
+      </div>
+    </div>
   )
 }
 
